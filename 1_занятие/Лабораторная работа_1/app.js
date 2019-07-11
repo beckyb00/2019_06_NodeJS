173552d33cdf32ddf9ceb177412375c0c5a70897
becky_b00 <becky_boo@mail.ru>
2019-07-12 03:12:36 +0400

    задание_1,2,3

--- 1_занятие/Лабораторная работа_1/app.js
+++ 1_занятие/Лабораторная работа_1/app.js
@@ -4,16 +4,36 @@ const fs = require('fs');
 
 http.createServer((request, response) => 
 {
-    fs.appendFile ("header.html", "body.html", "footer.html", "utf8", (err, data) => {
+    let allData='';
+    fs.readFile ('header.html', "utf8", (err, data) => {
         if (err) {
             console.log('Could not find or open file for reading\n');
             response.statusCode = 404;
             response.end();
         } else {
-            console.log(`The file ${data} is read and sent to the client\n`);
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(data);
-        }
+            allData += data;
+            fs.readFile ('body.html', "utf8", (err, data) => {
+                if (err) {
+                    console.log('Could not find or open file for reading\n');
+                    response.statusCode = 404;
+                    response.end();
+                } else {
+                    allData += data;
+                    fs.readFile ('footer.html', "utf8", (err, data) => {
+                        if (err) {
+                            console.log('Could not find or open file for reading\n');
+                            response.statusCode = 404;
+                            response.end();
+                        } else {
+                            allData += data;
+                            console.log(`The file ${data} is read and sent to the client\n`);
+                            response.writeHead(200, {'Content-Type':'text/html'});
+                            response.end(data);
+                        }
+                    });
+                }
+            });
+        }  
     });
     console.log("Request accepted!");
 }).listen(8080, ()=>{
